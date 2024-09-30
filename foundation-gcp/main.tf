46ffe547ad21a024ccd290dec23781a108e10f51
Johanu GANDONOU <johanugandonou@gmail.com>
2024-09-30 12:00:52 +0200

    Creation de l'instance et du database

--- foundation-gcp/main.tf
+++ foundation-gcp/main.tf
@@ -1,5 +1,51 @@
+variable "project_id" {
+    type = string
+    default = "esirem"
+}
+
+variable "region" {
+    type = string
+    default = "europe-west9"
+}
+
 provider "google" {
     project = var.project_id
     credentials = "./student.json"
     region  = var.region
+}
+
+resource "google_compute_instance" "vpc-network" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = var.region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
 }
